8c84569c2c1a1b1f31955ab598ab0346a56aeccd
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-23 10:47:02 -0700

    Added two more forms for Name and Team

--- src/components/DriverForm.js
+++ src/components/DriverForm.js
@@ -2,8 +2,14 @@ const DriverForm = (props) => {
     return(
         <form>
             {/* Good semantic HTML, keeps all forms together in one. Put a label for each input form */}
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name"/>
+
             <label htmlFor="country">Country</label>
             <input type="text" name="country"/>
+
+            <label htmlFor="team">Team</label>
+            <input type="text" name="team"/>
             
         </form>
     );
