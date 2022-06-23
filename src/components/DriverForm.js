64289a348b4f5be9b3aadfd48e01c3ef254895ae
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-23 10:53:55 -0700

    default values for Driver

--- src/components/DriverForm.js
+++ src/components/DriverForm.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
+
+const defaultDriver = {
+    name: '',
+    country: '',
+    team: '',
+    handsome: true
+};
+
 const DriverForm = (props) => { 
+    // State will hold one object 
+    const [formData, setFormData] = useState(defaultDriver);
+
     return(
         <form>
             {/* Good semantic HTML, keeps all forms together in one. Put a label for each input form */}
             <label htmlFor="name">Name</label>
-            <input type="text" name="name"/>
+            <input type="text" name="name" value={formData.name}/>
 
             <label htmlFor="country">Country</label>
-            <input type="text" name="country"/>
+            <input type="text" name="country" value={formData.country}/>
 
             <label htmlFor="team">Team</label>
-            <input type="text" name="team"/>
+            <input type="text" name="team" value={formData.team}/>
             
         </form>
     );
